900db51ca321d74ff4e83d949c0aaa05b61cc2de
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-04-20 11:10:28 +0530

    base url update

--- src/pages/avatarPage/AvatarPage.jsx
+++ src/pages/avatarPage/AvatarPage.jsx
@@ -92,7 +92,7 @@ export default function AvatarPage({
 
         try {
           axios
-            .post("https://b254-103-17-110-127.ngrok-free.app/rec", {
+            .post("https://h.ngrok.dev/rec", {
               image: capturedImg.split(",")[1],
               choice: base64Data.split(",")[1],
             })
